b6270fcbadcf465a220f7a024cc2a99f163ffe78
WangZhisong <2659460681@qq.com>
2022-10-20 10:40:39 +0800

    优化代码

--- service/collectionService.go
+++ service/collectionService.go
@@ -1,5 +1,12 @@
 package service
 
+import (
+	"Collection/utils/file"
+	"GinHello/mapper"
+	"GinHello/message/mqtt/producer"
+	"strings"
+)
+
 var (
 	CollectionServ = &CollectionService{}
 )
@@ -8,7 +15,13 @@ type CollectionService struct {
 }
 
 func (t *CollectionService) sendMqtt() (err error) {
-	//offset := file.ReadOffset()
-
+	//读取offset
+	offset := file.ReadOffset()
+	//拼接sql
+	var build strings.Builder
+	build.WriteString("" + offset)
+	mapper.SqlSession.Exec(build.String())
+	//发送mqtt
+	producer.Produce("")
 	return
 }
